db2f425aa958c5bf24f9f1e5d1e9df4a52b3a59f
Nestor Perez <nestor.perez@wolox.co>
2020-12-28 14:33:55 -0500

    ## Summary - Solve Pr notes, replace wildcards for imports in the BookControllerTest and UserControllerTest for specific methods ## Trello Card https://trello.com/c/WfOaZbtp/28-junit-testing

--- src/test/java/com/wolox/training/controller/UserControllerTest.java
+++ src/test/java/com/wolox/training/controller/UserControllerTest.java
@@ -26,7 +26,11 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
