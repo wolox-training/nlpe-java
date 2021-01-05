c84877a1e5c433599a6b69fff8fa1da532555976
Nestor Perez <nestor.perez@wolox.co>
2021-01-05 17:39:55 -0500

    ## Summary - Wiremock for book controller test - Mocks json file for responses - Add lombok dependency in gradle ## Trello Card https://trello.com/c/HPh0E8o1/43-mockear-respuestas-con-wiremock https://trello.com/c/f4ynI4rO/44-lombok-para-evitar-boiler-plate-code

--- src/test/java/com/wolox/training/controller/BookControllerTest.java
+++ src/test/java/com/wolox/training/controller/BookControllerTest.java
@@ -1,12 +1,18 @@
 package com.wolox.training.controller;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.wolox.training.dto.BookDTO;
 import com.wolox.training.exception.BookNotFoundException;
 import com.wolox.training.models.Book;
 import com.wolox.training.repository.BookRepository;
 import com.wolox.training.service.AuthService;
 import com.wolox.training.service.OpenLibraryService;
+import org.json.JSONObject;
+import org.junit.Rule;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -18,6 +24,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,17 +32,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.framework;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -60,6 +71,9 @@ public class BookControllerTest {
     @MockBean
     private PasswordEncoder passwordEncoder;
 
+    @Rule
+    private static WireMockRule wireMockRule = new WireMockRule(8080);
+    
     private final String PATH = "/api/book";
     private final String SPRING_USER = "spring";
     private final int id = 1;
@@ -84,6 +98,16 @@ public class BookControllerTest {
         given(passwordEncoder.matches(SPRING_PASSWORD, SPRING_PASSWORD)).willReturn(true);
     }
 
+    @BeforeAll
+    public static void beforeAll() {
+        wireMockRule.start();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        wireMockRule.stop();
+    }
+
     @WithMockUser(value = SPRING_USER)
     @Test
     public void givenBooks_whenGetAll_thenReturnArray() throws Exception {
@@ -92,7 +116,7 @@ public class BookControllerTest {
         Page<Book> page = new PageImpl<>(books, pageRequest, 10);
         given(bookRepository.findAll(any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any())).willReturn(page);
 
-        mvc.perform(get(PATH)
+        mvc.perform(MockMvcRequestBuilders.get(PATH)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$['pageable']['paged']").value("true"));
@@ -104,7 +128,7 @@ public class BookControllerTest {
         Book b = this.mockBooks().get(0);
         given(bookRepository.findByIsbn(b.getIsbn())).willReturn(Optional.of(b));
 
-        mvc.perform(get(PATH + "/" + b.getIsbn())
+        mvc.perform(MockMvcRequestBuilders.get(PATH + "/" + b.getIsbn())
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(res -> assertEquals(new BookDTO(b).toString(), res.getResponse().getContentAsString()));
@@ -113,13 +137,19 @@ public class BookControllerTest {
     @WithMockUser(value = SPRING_USER)
     @Test
     public void givenIsbn_whenGetBookByIsbn_thenCreateAndReturnBook() throws Exception {
+
+        stubFor(WireMock.get(urlEqualTo("https://openlibrary.org/api/books"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withBodyFile("mock/books.json")));
+
         Book b = this.mockBooks().get(0);
         BookDTO dto = new BookDTO(b);
         given(bookRepository.findByIsbn(b.getIsbn())).willReturn(Optional.empty());
         given(openLibraryService.bookInfo(b.getIsbn())).willReturn(dto);
         given(bookRepository.save(b)).willReturn(b);
 
-        mvc.perform(get(PATH + "/" + b.getIsbn())
+        mvc.perform(MockMvcRequestBuilders.get(PATH + "/" + b.getIsbn())
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(res -> assertEquals(dto.toString(), res.getResponse().getContentAsString()));
@@ -128,11 +158,17 @@ public class BookControllerTest {
     @WithMockUser(value = SPRING_USER)
     @Test
     public void givenIsbn_whenGetBookByIsbn_thenReturnBookNotFound() throws Exception {
+
+        stubFor(WireMock.get(urlEqualTo("https://openlibrary.org/api/books"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withBody("{}")));
+
         Book b = this.mockBooks().get(0);
         given(bookRepository.findByIsbn(b.getIsbn())).willReturn(Optional.empty());
         given(openLibraryService.bookInfo(b.getIsbn())).willThrow(BookNotFoundException.class);
 
-        mvc.perform(get(PATH + "/" + b.getIsbn())
+        mvc.perform(MockMvcRequestBuilders.get(PATH + "/" + b.getIsbn())
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
     }
@@ -142,7 +178,7 @@ public class BookControllerTest {
         Book b = this.mockBooks().get(0);
         given(bookRepository.save(any())).willReturn(b);
 
-        mvc.perform(post(PATH)
+        mvc.perform(MockMvcRequestBuilders.post(PATH)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .content(b.toString()))
@@ -155,7 +191,7 @@ public class BookControllerTest {
         Book b = this.mockBooks().get(0);
         given(bookRepository.save(any())).willThrow(DataIntegrityViolationException.class);
 
-        mvc.perform(post(PATH)
+        mvc.perform(MockMvcRequestBuilders.post(PATH)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .content(b.toString()))
@@ -169,7 +205,7 @@ public class BookControllerTest {
         given(bookRepository.findById(id)).willReturn(Optional.of(b));
         given(bookRepository.save(any())).willReturn(b);
 
-        mvc.perform(put(PATH + "/" + id)
+        mvc.perform(MockMvcRequestBuilders.put(PATH + "/" + id)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .content(body))
@@ -181,7 +217,7 @@ public class BookControllerTest {
     @Test
     public void givenBookAndId_whenUpdateBook_thenReturnIdMissMatch() throws Exception {
 
-        mvc.perform(put(PATH + "/" + 50)
+        mvc.perform(MockMvcRequestBuilders.put(PATH + "/" + 50)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .content(body))
@@ -193,7 +229,7 @@ public class BookControllerTest {
     public void givenBookAndId_whenUpdateBook_thenReturnBookNotFound() throws Exception {
         given(bookRepository.findById(id)).willReturn(Optional.empty());
 
-        mvc.perform(put(PATH + "/" + id)
+        mvc.perform(MockMvcRequestBuilders.put(PATH + "/" + id)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .content(body))
@@ -208,7 +244,7 @@ public class BookControllerTest {
         given(bookRepository.findById(id)).willReturn(Optional.of(b));
         doNothing().when(bookRepository).deleteById(any());
 
-        mvc.perform(delete(PATH + "/" + id)
+        mvc.perform(MockMvcRequestBuilders.delete(PATH + "/" + id)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
@@ -220,7 +256,7 @@ public class BookControllerTest {
 
         given(bookRepository.findById(id)).willReturn(Optional.empty());
 
-        mvc.perform(delete(PATH + "/" + id)
+        mvc.perform(MockMvcRequestBuilders.delete(PATH + "/" + id)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
